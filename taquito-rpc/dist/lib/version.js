3fbf8f377a8b32ede33918a124fd8c00f98f56c1
Clement Renaud <clement@clementrenaud.com>
2021-11-06 18:44:25 +0100

    taquito rpc

---
+++ taquito-rpc/dist/lib/version.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VERSION = void 0;
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+exports.VERSION = {
+    "commitHash": "0603bd33ddcf58151615fffb21ab19400fa54d38",
+    "version": "10.2.0"
+};
+/* tslint:enable */
+//# sourceMappingURL=version.js.map
